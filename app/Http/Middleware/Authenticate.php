a90026455450a0373a0d7f9c91af18b7d5679510
wilfran <wilfranr@gmail.com>
2024-11-04 18:02:10 -0500

    feat: cambiar la redirección de autenticación a la página de inicio

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,6 +12,6 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        return $request->expectsJson() ? null : '/home'; // Cambia 'login' por '/home' o la ruta que prefieras
     }
 }
