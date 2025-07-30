43eb71f5cbfb6e193060fc9ca6f07e89813e5720
wilfran <wilfranr@gmail.com>
2025-07-29 21:03:47 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+        "./resources/**/*.blade.php",
+        "./vendor/filament/**/*.blade.php",
+        "./vendor/awcodes/filament-table-repeater/resources/**/*.blade.php", // añade esta línea
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
